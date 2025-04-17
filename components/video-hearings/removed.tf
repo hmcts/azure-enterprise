af6bd7b4f74bf1a499529e70950c5263871915bb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-17 10:23:49 +0100

    test workaround

--- components/video-hearings/removed.tf
+++ components/video-hearings/removed.tf
@@ -1,5 +1,10 @@
-removed {
+moved {
   from = module.subscription["DTS-VH-PROD"].azurerm_subscription.this
+  to = module.subscription.azurerm_subscription.this_dts_vh_prod
+}
+
+removed {
+  from = module.subscription.azurerm_subscription.this_dts_vh_prod
 
   lifecycle {
     destroy = false
