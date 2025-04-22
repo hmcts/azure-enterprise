30e796a063100cbd52ef6e1737e36cf61d6ce834
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-22 15:45:30 +0100

    add more removed resources

--- components/video-hearings/removed.tf
+++ components/video-hearings/removed.tf
@@ -1,10 +1,37 @@
-moved {
-  from = module.subscription["DTS-VH-PROD"].azurerm_subscription.this
-  to   = module.subscription.azurerm_subscription.this_dts_vh_prod
+removed {
+  from = module.subscription["DTS-VH-PROD"].azurerm_key_vault_secret.sp_object_id
+
+  lifecycle {
+    destroy = false
+  }
 }
 
 removed {
-  from = module.subscription.azurerm_subscription.this_dts_vh_prod
+  from = module.subscription["DTS-VH-PROD"].azurerm_key_vault_secret.sp_app_id
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.subscription["DTS-VH-PROD"].azurerm_key_vault_secret.sp_token
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.subscription["DTS-VH-PROD"].azurerm_key_vault_secret.aks_admin_group_id
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.subscription["DTS-VH-PROD"].azurerm_key_vault_secret.aks_user_group_id
 
   lifecycle {
     destroy = false
