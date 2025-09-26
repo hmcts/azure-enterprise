91e6d98aac490e6ab49e0b14d40df6b6cedb61c9
Marty Fox <martin.fox1@justice.gov.uk>
2025-09-26 09:55:53 +0100

    undo storage change

--- modules/subscription/storage.tf
+++ modules/subscription/storage.tf
@@ -17,6 +17,6 @@ resource "azurerm_storage_account" "sa" {
 
 resource "azurerm_storage_container" "sc" {
   name                  = var.storage_container_name
-  storage_account_id    = azurerm_storage_account.sa.id
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = var.access_type
 }
