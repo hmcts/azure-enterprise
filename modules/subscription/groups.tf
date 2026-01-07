3818e790cde04a23bd7365b7406b972bdd6875d2
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-07 17:02:19 +0000

    fix logic

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -1,15 +1,18 @@
-# Keep the original groups resource unchanged
 resource "azuread_group" "groups" {
   for_each                = local.groups
-  display_name            = each. value.name
+  display_name            = each.value.name
   description             = each.value.description
   prevent_duplicate_names = true
+  security_enabled        = true
 }
 
-# Add a new resource for conditional groups
-resource "azuread_group" "conditional_groups" {
-  for_each                = local.conditional_groups
-  display_name            = each. value.name
-  description             = each.value.description
-  prevent_duplicate_names = true
+resource "azuread_group_member" "members" {
+  for_each         = local.members_list
+  group_object_id  = azuread_group.groups[each.value.role].object_id
+  member_object_id = each.value.member
+}
+
+resource "azuread_group_member" "dts_operation_members" {
+  group_object_id  = data.azuread_group.dts_operations.object_id
+  member_object_id = azuread_service_principal.sp.object_id
 }
