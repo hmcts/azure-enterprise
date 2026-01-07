f78a0d9302eede983961b09bc0ea7595c344920f
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-07 16:19:22 +0000

    edit logic with minimal changes

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -1,5 +1,5 @@
 resource "azuread_group" "groups" {
-  for_each                = local.all_groups
+  for_each                = local.groups
   display_name            = each.value.name
   description             = each.value.description
   prevent_duplicate_names = true
@@ -9,10 +9,10 @@ resource "azuread_group" "groups" {
 resource "azuread_group_member" "members" {
   for_each         = local.members_list
   group_object_id  = azuread_group.groups[each.value.role].object_id
-  member_object_id = each.value.member
+  member_object_id = each.value. member
 }
 
 resource "azuread_group_member" "dts_operation_members" {
-  group_object_id  = data. azuread_group.dts_operations.object_id
-  member_object_id = azuread_service_principal.sp. object_id
+  group_object_id  = data.azuread_group. dts_operations.object_id
+  member_object_id = azuread_service_principal. sp.object_id
 }
