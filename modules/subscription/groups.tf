dd6d1df3da088ef002610e17e24fdc07f5c1043e
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-08 09:46:55 +0000

    remove new azure ad group

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -6,14 +6,6 @@ resource "azuread_group" "groups" {
   security_enabled        = true
 }
 
-resource "azuread_group" "conditional_groups" {
-  for_each                = local.conditional_groups
-  display_name            = each.value.name
-  description             = each.value.description
-  prevent_duplicate_names = true
-  security_enabled        = true
-}
-
 resource "azuread_group_member" "members" {
   for_each         = local.members_list
   group_object_id  = azuread_group.groups[each.value.role].object_id
