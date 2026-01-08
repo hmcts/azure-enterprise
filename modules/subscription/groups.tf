df4ef8201c6ecb05d5bce9536f4fc85c1a719b83
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-08 11:40:31 +0000

    Revert changes to local.tf

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -16,3 +16,21 @@ resource "azuread_group_member" "dts_operation_members" {
   group_object_id  = data.azuread_group.dts_operations.object_id
   member_object_id = azuread_service_principal.sp.object_id
 }
+
+locals {
+  create_contributor_eligible = !strcontains(lower(azurerm_subscription.this.subscription_name), "sandbox") && !strcontains(lower(azurerm_subscription.this.subscription_name), "sbox")
+}
+
+resource "azuread_group" "contributor_eligible" {
+  for_each = local.create_contributor_eligible ? {
+    "Contributor Eligible" = {
+      name        = "DTS Contributors Eligible (sub:${lower(azurerm_subscription.this.subscription_name)})"
+      description = "Holds users eligible for Contributor access via access packages for ${azurerm_subscription.this.subscription_name} subscription."
+    }
+  } : {}
+
+  display_name            = each.value.name
+  description             = each.value.description
+  prevent_duplicate_names = true
+  security_enabled        = true
+}
