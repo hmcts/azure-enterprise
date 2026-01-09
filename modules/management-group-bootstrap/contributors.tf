633f93e79d5b76a032d148a966ebba904fe28e9f
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-09 09:58:03 +0000

    test hardcore object id of pim_approvers

--- modules/management-group-bootstrap/contributors.tf
+++ modules/management-group-bootstrap/contributors.tf
@@ -18,7 +18,7 @@ resource "azurerm_role_assignment" "contributors" {
 
 # Data source to lookup the existing PIM approvers group
 data "azuread_group" "pim_approvers" {
-  display_name = "DTS Azure PIM Approvers (CNP)"
+  object_id = "cfdbb1cc-e789-4d2c-b390-1d9ed77603d3"
 }
 
 # Assign Contributor role to PIM Approvers group at Prod level for emergency access
