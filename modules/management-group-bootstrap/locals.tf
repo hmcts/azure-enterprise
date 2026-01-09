749deb1d7b08bf699b4c80172180564783cc18b5
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-09 13:49:53 +0000

    add space

--- modules/management-group-bootstrap/locals.tf
+++ modules/management-group-bootstrap/locals.tf
@@ -5,28 +5,28 @@ locals {
 
   group_assignment_product_readers = setproduct(keys(var.groups), local.custom_role_assignments_readers)
 
-  # Environment detection
-  is_sandbox_environment = anytrue([
+  has_prod_groups = anytrue([
     for k, mg in var.groups :
-    can(regex("(?i)(sandbox|sbox)", mg.display_name))
+    strcontains(lower(mg.display_name), "prod") ||
+    strcontains(lower(mg.display_name), "production")
   ])
 
-  is_prod_environment = !local.is_sandbox_environment
+  detected_environment = local.has_prod_groups ? "prod" : "sandbox"
 
-  environment_group_ids = {
+  is_prod_environment    = local.detected_environment == "prod"
+  is_sandbox_environment = local.detected_environment == "sandbox"
+
+  group_ids = {
     platform_ops = {
-      display_name = "DTS Platform Operations"
-      sandbox_id   = "9b200a9c-8c0b-497a-9246-2c9b4dcc0d02"
-      prod_id      = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
+      sandbox = "9b200a9c-8c0b-497a-9246-2c9b4dcc0d02"
+      prod    = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
     }
     pim_approvers = {
-      display_name = "DTS Azure PIM Approvers (CNP)"
-      sandbox_id   = "3e1fcd71-06ff-4531-a2fa-db6468830fda"
-      prod_id      = "cfdbb1cc-e789-4d2c-b390-1d9ed77603d3"
+      sandbox = "3e1fcd71-06ff-4531-a2fa-db6468830fda"
+      prod    = "cfdbb1cc-e789-4d2c-b390-1d9ed77603d3"
     }
   }
 
-  platform_ops_group_id = local.is_sandbox_environment ? local.environment_group_ids.platform_ops.sandbox_id : local.environment_group_ids.platform_ops.prod_id
-
-  pim_approvers_group_id = local.is_sandbox_environment ? local.environment_group_ids.pim_approvers.sandbox_id : local.environment_group_ids.pim_approvers.prod_id
+  platform_ops_group_id  = local.group_ids.platform_ops[local.detected_environment]
+  pim_approvers_group_id = local.group_ids.pim_approvers[local.detected_environment]
 }
