dbfd194b437b1b98d24fb490839a26bb5cf22bae
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-01-09 14:11:20 +0000

    fix logic to work for prod

--- modules/management-group-bootstrap/locals.tf
+++ modules/management-group-bootstrap/locals.tf
@@ -5,14 +5,22 @@ locals {
 
   group_assignment_product_readers = setproduct(keys(var.groups), local.custom_role_assignments_readers)
 
-  is_sandbox_environment = anytrue([
+  sandbox_environment = alltrue([
     for k, mg in var.groups :
     can(regex("(?i)(sandbox|sbox)", mg.display_name))
+  ]) && !anytrue([
+    for k, mg in var.groups :
+    can(regex("(?i)prod", mg.display_name))
   ])
 
-  is_prod_environment = !local.is_sandbox_environment
+  has_production_groups = anytrue([
+    for k, mg in var.groups :
+    can(regex("(?i)prod", mg.display_name))
+  ])
 
-  # Group ID mappings
+  is_sandbox_environment = local.sandbox_environment
+  is_prod_environment    = !local.is_sandbox_environment
+
   group_ids = {
     platform_ops = {
       sandbox = "9b200a9c-8c0b-497a-9246-2c9b4dcc0d02"
@@ -24,8 +32,7 @@ locals {
     }
   }
 
-  # Select appropriate IDs based on environment
-  platform_ops_group_id = local.is_sandbox_environment ? local.group_ids.platform_ops.sandbox : local.group_ids.platform_ops.prod
+  platform_ops_group_id  = local.is_sandbox_environment ? local.group_ids.platform_ops.sandbox : local.group_ids.platform_ops.prod
 
   pim_approvers_group_id = local.is_sandbox_environment ? local.group_ids.pim_approvers.sandbox : local.group_ids.pim_approvers.prod
 }
