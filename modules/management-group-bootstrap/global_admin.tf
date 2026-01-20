c5d55bfe8e447d8624fdefc3c6260fe4ebed386a
Adam McNeilly <adam.mcneilly@kainos.com>
2026-01-20 10:36:34 +0000

    Slight text tweak

--- modules/management-group-bootstrap/global_admin.tf
+++ modules/management-group-bootstrap/global_admin.tf
@@ -1,6 +1,6 @@
 # Eligible Global Administrator Group
 resource "azuread_group" "global_admin_eligible" {
-  display_name            = "DTS Global Admin Eligible"
+  display_name            = "DTS Global Admins Eligible"
   description             = "Holds users eligible for Global Admin access via PIM access packages."
   prevent_duplicate_names = true
   security_enabled        = true
@@ -8,8 +8,8 @@ resource "azuread_group" "global_admin_eligible" {
 
 # Active Global Administrator Group
 resource "azuread_group" "global_admin" {
-  display_name            = "DTS Global Administrators"
-  description             = "Active Global Administrators with Owner permissions at tenant root."
+  display_name            = "DTS Global Admins"
+  description             = "Active Global Admins with Owner permissions at tenant root."
   prevent_duplicate_names = true
   security_enabled        = true
 }
