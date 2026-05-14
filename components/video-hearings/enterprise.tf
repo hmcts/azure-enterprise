4aaf50f54bacbf5da5c845f5989b5b58655e71aa
charlesluokb <48835874+charlesluokb@users.noreply.github.com>
2026-05-14 16:53:51 +0000

    Apply automatic changes

--- components/video-hearings/enterprise.tf
+++ components/video-hearings/enterprise.tf
@@ -1,10 +1,10 @@
 module "enterprise" {
   source = "../../modules/enterprise"
 
-  root_id        = var.root_id
-  root_name      = var.root_name
-  root_parent_id = data.azurerm_client_config.core.tenant_id
-  env            = var.env
+  root_id             = var.root_id
+  root_name           = var.root_name
+  root_parent_id      = data.azurerm_client_config.core.tenant_id
+  env                 = var.env
   create_custom_roles = var.create_custom_roles
   platform_operations = var.platform_operations
   pim_approvers       = var.pim_approvers
