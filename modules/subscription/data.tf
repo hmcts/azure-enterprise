20d803e9bb10dffc780f2c2f7e1fb61534a8f1d3
kainosrogery <140807585+kainosrogery@users.noreply.github.com>
2026-06-26 15:42:35 +0000

    Apply automatic changes

--- modules/subscription/data.tf
+++ modules/subscription/data.tf
@@ -15,5 +15,5 @@ data "azuread_group" "dts_operations" {
 data "azuread_application_published_app_ids" "well_known" {}
 
 data "azuread_service_principal" "graph_api_principal" {
-client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+  client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
 }
